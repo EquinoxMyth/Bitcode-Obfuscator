87eb8a122a6270e37ed16f72e4e05b95a9e40eca
Mythra <65871266+EquinoxMyth@users.noreply.github.com>
2021-02-22 10:39:48 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ local player = game.Players.LocalPlayer
 local scriptowner = player.Name
 local scriptownerid = player.UserId
 local backend = print
-local obfuname = "BitCode"
+local obfuname = "BiteCode"
 copytoclipboard = false; -- change this if you want it to copy to clipboard only works for executors 
 -----------------------------
 
