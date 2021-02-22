63d87eff29f50dc35fbf89b44a31a2dbc121d5ab
Mythra <65871266+EquinoxMyth@users.noreply.github.com>
2021-02-22 10:05:56 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,9 @@
 --init--
 local player = game.Players.LocalPlayer
-scriptowner = player.Name
-scriptownerid = player.UserId
-backend = print
+local scriptowner = player.Name
+local scriptownerid = player.UserId
+local backend = print
+local obfuname = "BitCode"
 -----------------------------
 
 
@@ -13,19 +14,13 @@ local w = g.Workspace
 print(w:GetChildren())
 ]]
 ------
-local backend = [[
 
---{{
- -- Made With  [your obfuscator name] Obfuscator - Version 0.1,
---"Enjoy Exploting Games!"
 
---}}
-
-]]
 
 local function obfuscate(text)
 msg = text
 local result = msg:gsub(".", function(incode) return "\\" .. incode:byte () end) or script .."\\\""
+ backend('created with: ['..obfuname..']  Obfuscator')
 backend("Made By: "..scriptowner.." Id: "..scriptownerid)
 wait(0.1)
 backend("loadstring('"..result.."')(true)")
