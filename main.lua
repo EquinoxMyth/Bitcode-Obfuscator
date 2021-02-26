ded1873be1aac57396af7845b67a612ae3a8883f
Mythra <65871266+EquinoxMyth@users.noreply.github.com>
2021-02-26 11:08:04 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 
 --init--
-local player = game.Players.LocalPlayer
-local scriptowner = player.Name
-local scriptownerid = player.UserId
+--local player = game.Players.LocalPlayer
+--local scriptowner = player.Name
+--local scriptownerid = player.UserId
 local backend = print
-local obfuname = "BiteCode"
+local obfuname = "BiteCode";
+local owner = "Player1";
+local version =  "0.0.1";
 copytoclipboard = false; -- change this if you want it to copy to clipboard only works for executors / suggest using Synapse or Fluxus or Jellyfish or ScriptWare
 -----------------------------
 
@@ -19,19 +21,19 @@ local function obfuscate(text)
 msg = text
 local result = msg:gsub(".", function(incode) return "\\" .. incode:byte () end) or script .."\\\""
  backend('created with: ['..obfuname..']  Obfuscator')
-backend("Made By: "..scriptowner.." Id: "..scriptownerid)
-wait(0.1)
-if copytoclipboard = false then
-backend"loadstring('"..result.."')()"
+backend("Made By: "..owner.." Id: ") -- add ..scriptowner or scriptownerid 
+--- Add a wait here if you want to.
+if copytoclipboard == false then
+backend("loadstring("..result..")()")
   backend'Printed To Console'
   util = result
   else
-  if copytoclipboard = true then
-   setclipboard("loadstring('"..result.."')()")
+  if copytoclipboard == true then
+   setclipboard("loadstring("..result..")()")
      backend'Copied To Clipboard'
    end
   end
-end)
+end
 -----------
 
 ----Other Part
@@ -48,6 +50,5 @@ print("Hello World")
 --Myth Dev#3 Patch tester
 
 --All Of The Myth Team Are Exploit Patchers/ We Only Exploit To Test Our Knowledge of Lua.
-
 
 
